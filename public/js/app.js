eab41c02b4f5b34ce22d96aa9eabb2a013a4caeb
David Jimenez <d.jimenez@sap.com>
2013-08-19 13:18:31 +0100

    Create a small blog app
    Signed-off-by: David Jimenez <d.jimenez@sap.com>

--- public/js/app.js
+++ public/js/app.js
@@ -2,25 +2,31 @@
 
 // Declare app level module which depends on filters, and services
 
-angular.module('myApp', [
-  'myApp.controllers',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives'
-]).
-config(function ($routeProvider, $locationProvider) {
+angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
+config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
   $routeProvider.
-    when('/view1', {
-      templateUrl: 'partials/partial1',
-      controller: 'MyCtrl1'
+    when('/', {
+      templateUrl: 'partials/index',
+      controller: IndexCtrl
     }).
-    when('/view2', {
-      templateUrl: 'partials/partial2',
-      controller: 'MyCtrl2'
+    when('/addPost', {
+      templateUrl: 'partials/addPost',
+      controller: AddPostCtrl
+    }).
+    when('/readPost/:id', {
+      templateUrl: 'partials/readPost',
+      controller: ReadPostCtrl
+    }).
+    when('/editPost/:id', {
+      templateUrl: 'partials/editPost',
+      controller: EditPostCtrl
+    }).
+    when('/deletePost/:id', {
+      templateUrl: 'partials/deletePost',
+      controller: DeletePostCtrl
     }).
     otherwise({
-      redirectTo: '/view1'
+      redirectTo: '/'
     });
-
   $locationProvider.html5Mode(true);
-});
+}]);
